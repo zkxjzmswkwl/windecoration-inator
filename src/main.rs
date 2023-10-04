90baab0d5d0aa9afd2d66d7eb65c3c3118ba3703
vablings <47798035+vablings@users.noreply.github.com>
2023-10-04 13:35:10 +0100

    i love soy

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+
+#![windows_subsystem = "windows"]
 use core::time;
 use num_traits::PrimInt;
-use std::process::exit;
+use std::{process::exit, sync::mpsc};
 use std::thread;
-
+use tray_item::{IconSource, TrayItem};
 use winapi::um::winuser::{
     GetAsyncKeyState, GetForegroundWindow, GetWindowLongPtrA, SetWindowLongPtrA, UpdateWindow,
     GWL_STYLE, WS_BORDER, WS_CAPTION, WS_SIZEBOX,
 };
+
+enum Message {
+    Quit,
+}
 
 unsafe fn remove_window_decorations(style_to_remove: u32) {
     let hwnd = GetForegroundWindow();
@@ -21,6 +27,20 @@ unsafe fn remove_window_decorations(style_to_remove: u32) {
 }
 
 fn main() {
+
+    let mut tray = TrayItem::new(
+        "windecoration-inator",
+        IconSource::Resource("really-cool-icon"),
+    )
+    .unwrap();
+    tray.add_label("LEFT ALT + Q: Toggle decorations on active window").unwrap();
+    tray.add_label("LEFT ALT + Z: Exit program").unwrap();
+    let (tx, rx) = mpsc::sync_channel(1);
+    let quit_tx = tx.clone();
+    tray.add_menu_item("Quit", move || {
+        quit_tx.send(Message::Quit).unwrap();
+    }).unwrap();
+
     println!("Keybinds:");
     println!("\tLEFT ALT + Q: Toggle decorations on active window\t");
     println!("\tLEFT ALT + Z: Exit program");
@@ -46,6 +66,13 @@ fn main() {
             if msb(GetAsyncKeyState(0xA4)) > 0 && msb(GetAsyncKeyState(0x5A)) > 0 & 1 {
                 println!("?");
                 break;
+            }
+            match rx.try_recv() {
+                Ok(Message::Quit) => {
+                    println!("Quit");
+                    break;
+                }
+                _ => {}
             }
         }
         thread::sleep(time::Duration::from_millis(20));
